2709a72c1e1a2efdf6cbad7b021f91a6db29eff3
JivkoJelev91 <zhelev.zhivko.1991@gmail.com>
2018-08-29 15:55:03 +0300

    Change about function

--- src/components/terminal.vue
+++ src/components/terminal.vue
@@ -153,7 +153,7 @@ export default {
       this.info.map((text, index) => {
         setTimeout(() => {
           this.arr.push(text);
-        }, index * 100);
+        }, index * 500);
       });
     },
 
